3c8545c808a1cb71aedb5a6e1858ae2bba55037b
daliang.song <wsdy007@sina.com>
2020-12-04 10:32:01 +0800

    加上对错号来配合扬声器开关。

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+def on_button_pressed_a():
+    music.set_built_in_speaker_enabled(True)
+    basic.show_icon(IconNames.YES)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+music.set_built_in_speaker_enabled(False)
+basic.show_icon(IconNames.NO)
+
 def on_forever():
-    pass
+    music.play_melody("C C G G A A G - ", 120)
+    music.play_melody("F F E E D D C - ", 120)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     music.setBuiltInSpeakerEnabled(true)
+    basic.showIcon(IconNames.Yes)
 })
 music.setBuiltInSpeakerEnabled(false)
-basic.showIcon(IconNames.Heart)
+basic.showIcon(IconNames.No)
 basic.forever(function () {
     music.playMelody("C C G G A A G - ", 120)
     music.playMelody("F F E E D D C - ", 120)
